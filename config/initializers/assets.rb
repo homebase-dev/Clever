b3fe9d55bd1f8d7efe603707907e0942e86673b4
homebase.dev <homebase.dev@gmail.com>
2015-01-17 21:01:29 +0100

    Fixes ckeditor assets (they are added to precompilation now), also glpyhicons, but they still don't work

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -6,3 +6,7 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
+
+# Add ckeditor files - otherwise missing on production  Rails.application.config.assets.precompile += %w( ckeditor/* )
+# Add railsstrap glyphicons - otherwise missing on production   Rails.application.config.assets.precompile += %w( fontawesome/fonts/fontawesome-webfont.eot fontawesome/fonts/fontawesome-webfont.woff fontawesome/fonts/fontawesome-webfont.ttf fontawesome/fonts/fontawesome-webfont.svg)
+Rails.application.config.assets.precompile += %w( ckeditor/* bootstrap/fonts/* fontawesome/fonts/* )
