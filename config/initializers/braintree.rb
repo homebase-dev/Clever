a3c48d206d48c7810fe77bfb69bf7a35b69c689c
Christian Hofer <homebase.dev@gmail.com>
2015-05-25 14:02:48 +0200

    Improves news section on home page and adds braintree sandbox data

--- config/initializers/braintree.rb
+++ config/initializers/braintree.rb
@@ -1,4 +1,4 @@
 Braintree::Configuration.environment = :sandbox
-Braintree::Configuration.merchant_id = ""
-Braintree::Configuration.public_key = ""
-Braintree::Configuration.private_key = ""
+Braintree::Configuration.merchant_id = "3rtgfvcffkcyryhb"
+Braintree::Configuration.public_key = "qzxshskhtmh772s4"
+Braintree::Configuration.private_key = "59db74521c2d619f2248e0e6ec7b85aa"
