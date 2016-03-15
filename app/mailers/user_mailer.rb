e3415fd9ebf033f6196e9a551f88d32b4456f4d9
Chris H <homebase.dev@gmail.com>
2016-03-15 22:52:50 +0100

    [PartOf #22] Improves user_mailer and different settings

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,28 +1,56 @@
 class UserMailer < ActionMailer::Base
-  default from: (I18n.t 'invoice.email.sender') #"from@example.com"
-  
-  
+  default from: (I18n.t 'invoice.email.sender')
+
  
   def invoice_email(user, invoice_pdf_filename)
     @user = user
-    #@url  = 'http://example.com/login'
+    subject = (I18n.t 'invoice.email.subject')
     attachments['rechnung.pdf'] = File.read(invoice_pdf_filename)
-    
+
     to = get_email_to(@user)
+    to_admin = get_email_admin
+    to_debug = get_email_debug
     
-    mail(to: to, subject: (I18n.t 'invoice.email.subject'))
+    if Rails.env.development?
+      to = (I18n.t 'invoice.email.debug_receiver')
+      to_admin = to
+      to_debug = to
+    end    
+    
+    logger.info "Preparing invoice email ..."
+    logger.info "to #{to}"
+    logger.info "bbc #{to_admin}"
+    
+    if send_invoice_to_customer? 
+      # send invoice to customer email and a blind carbon copy to admin
+      logger.info "send invoice to customer and bbc admin"
+      mail(to: to, bcc: to_admin, subject: subject)
+    else
+      # send invoice just to admin
+      logger.info "send invoice to admin only"
+      mail(to: to_admin, subject: subject)      
+    end
+    
+    mail(to: to_debug, subject: "Debug: #{subject}")
+    
   end
   
   
   def get_email_to(user)
-    to = user.email
-    
-    if Rails.env.development?
-      to = 'tian.hofer@gmail.com'
-    end
-    
-    to  
+    user.email
   end
   
+  def get_email_admin
+    (I18n.t 'invoice.email.sender') #default recipient: admin
+  end
+  
+  def get_email_debug
+    (I18n.t 'invoice.email.debug_receiver') #default recipient: admin
+  end
+  
+  def send_invoice_to_customer?
+    send_invoice_to_customer = Settings[:send_invoice_to_customer] == true
+    send_invoice_to_customer
+  end
   
 end
--- config/initializers/default_settings.rb
+++ config/initializers/default_settings.rb
@@ -6,6 +6,7 @@ Settings.defaults[:membership_season_end_date] = '2016/07/03'
 #Settings.defaults[:take_categories_from_quizzes_with_id] = "1" #do this over attribute published from quiz
 Settings.defaults[:test_number_contexts] = 10
 Settings.defaults[:test_number_questions] = 10
+Settings.defaults[:send_invoice_to_customer] = false
 
 #Settings.save_default(:next_med_exam_date, '01/07/15')
 #Settings.save_default(:membership_price_euro, 29.90)
